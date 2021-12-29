e29bcde00257a76b07ceda1ebcd21531ae8c2a69
ABHAY TARAS <73850631+abhay-h@users.noreply.github.com>
2021-12-30 01:51:38 +0530

    Add files via upload

---
+++ FirstPro.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FirstPro{
+
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in); 
+      //  System.out.println("\tHello\n");
+         int plol= input.nextInt(); 
+    }
+
+
+                               
+}
